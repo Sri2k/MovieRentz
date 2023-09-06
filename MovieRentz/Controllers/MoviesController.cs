f581be6db51e61e3113f513a4280d77f5edda318
SriSai_G <gandusrisai@gmail.com>
2023-09-06 15:24:05 +0530

    attribute routing

---
+++ MovieRentz/Controllers/MoviesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieRentz.Models;
+
+namespace MovieRentz.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Random()
+        {
+            var movie = new Movie() { Name = "RRR" };
+            //return View(movie);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Edit(int movieId)
+        {
+            return Content($"Id: {movieId}");
+        }
+
+        public ActionResult Index(int? pageIndex,string sortBy)
+        {
+            if(!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+            return Content( $"pageIndex: {pageIndex} sortBy:{sortBy}" );
+        }
+        [Route("movies/released/{year}/{month}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);  
+        }
+    }
+}
--- MovieRentz/Program.cs
+++ MovieRentz/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Mvc;
+using MovieRentz.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
