865ddd082ec8db24685b08307fa7dedd9ad5b2c1
SriSai_G <gandusrisai@gmail.com>
2023-09-06 16:19:17 +0530

    navBar partial view and action methods added

--- MovieRentz/Controllers/MoviesController.cs
+++ MovieRentz/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MovieRentz.Models;
+using MovieRentz.ViewModels;
 
 namespace MovieRentz.Controllers
 {
@@ -8,31 +9,45 @@ namespace MovieRentz.Controllers
         public IActionResult Random()
         {
             var movie = new Movie() { Name = "RRR" };
-            //return View(movie);
-            return RedirectToAction("Index", "Home");
+            var customers = new List<Customer>()
+            {
+                new Customer{Name="Sri"},
+                new Customer{Name="Sai"},
+                new Customer{Name="Maddy"}
+
+            };
+
+            var randomMovieViewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            return View(randomMovieViewModel);
         }
 
-        public ActionResult Edit(int movieId)
-        {
-            return Content($"Id: {movieId}");
-        }
 
-        public ActionResult Index(int? pageIndex,string sortBy)
-        {
-            if(!pageIndex.HasValue)
-            {
-                pageIndex = 1;
-            }
-            if (String.IsNullOrWhiteSpace(sortBy))
-            {
-                sortBy = "Name";
-            }
-            return Content( $"pageIndex: {pageIndex} sortBy:{sortBy}" );
-        }
-        [Route("movies/released/{year}/{month}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);  
-        }
+
+        //public ActionResult Edit(int movieId)
+        //{
+        //    return Content($"Id: {movieId}");
+        //}
+
+        //public ActionResult Index(int? pageIndex,string sortBy)
+        //{
+        //    if(!pageIndex.HasValue)
+        //    {
+        //        pageIndex = 1;
+        //    }
+        //    if (String.IsNullOrWhiteSpace(sortBy))
+        //    {
+        //        sortBy = "Name";
+        //    }
+        //    return Content( $"pageIndex: {pageIndex} sortBy:{sortBy}" );
+        //}
+        //[Route("movies/released/{year}/{month}")]
+        //public ActionResult ByReleaseDate(int year, int month)
+        //{
+        //    return Content(year + "/" + month);  
+        //}
     }
 }
